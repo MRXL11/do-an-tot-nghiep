178921096f748de94fee5186a063eec3b6fb7333
luongtd15 <luongnek121@gmail.com>
2025-05-27 19:05:29 +0700

    create Admin/ProductController.php in app/Http/Controllers & Product.php in app/Models

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'sku',
+        'description',
+        'short_description',
+        'category_id',
+        'brand_id',
+        'thumbnail',
+        'status',
+    ];
+
+    // public function category()
+    // {
+    //     return $this->belongsTo(Category::class);
+    // }
+    // public function brand()
+    // {
+    //     return $this->belongsTo(Brand::class);
+    // }
+    // public function reviews()
+    // {
+    //     return $this->hasMany(Review::class);
+    // }
+}
