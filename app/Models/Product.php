f301ced5b3cb5a1bac0f5fc51fa1776ee2ed45a5
luongtd15 <luongnek121@gmail.com>
2025-05-27 19:21:12 +0700

    create class Brand, Category and modified Models/Product.php to get product's brand and category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,14 +22,14 @@ class Product extends Model
         'status',
     ];
 
-    // public function category()
-    // {
-    //     return $this->belongsTo(Category::class);
-    // }
-    // public function brand()
-    // {
-    //     return $this->belongsTo(Brand::class);
-    // }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
     // public function reviews()
     // {
     //     return $this->hasMany(Review::class);
