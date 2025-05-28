de066e67fb97fab14b4859ce20b25c2702394619
luongtd15 <luongnek121@gmail.com>
2025-05-28 14:10:08 +0700

    modified admin/products routes

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Http\Controllers\Admin\ProductController as AdminProductController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/admin', function () {
-    return view('admin.others_menu.statistical');  
+    return view('admin.others_menu.statistical');
 })->name('statistical');
 
 Route::get('/users', function () {
     return view('admin.users.users');
 })->name('users');
 
-Route::get('/products', function () {
-    return view('admin.products.products');
-})->name('products');
+Route::resource('/products', AdminProductController::class)->names('admin.products');
+Route::post('/admin/products/{id}/restore', [AdminProductController::class, 'restore'])->name('admin.products.restore');
+
 
 Route::get('/orders', function () {
     return view('admin.orders.orders');
@@ -40,45 +41,45 @@ Route::get('/notifications', function () {
 
 // đây là khách hàng
 Route::get('/', function () {
-   return view('client.layouts.index');
+    return view('client.layouts.index');
 });
-route ::get('/page', function () {
+route::get('/page', function () {
     return view('client.pages.page-layout');
 });
-route ::get('/products-client', function () {
+route::get('/products-client', function () {
     return view('client.pages.products-client');
 });
-route ::get('/cart', function () {
+route::get('/cart', function () {
     return view('client.pages.cart');
 });
-route ::get('/checkout', function () {
+route::get('/checkout', function () {
     return view('client.pages.checkout');
 });
-route ::get('/about', function () {
+route::get('/about', function () {
     return view('client.pages.about');
 });
 
-route ::get('/contact', function () {
+route::get('/contact', function () {
     return view('client.pages.contact');
 });
 
-route ::get('/wishlist', function () {
+route::get('/wishlist', function () {
     return view('client.pages.wishlist');
 });
 
-route ::get('/account', function () {
+route::get('/account', function () {
     return view('client.pages.account');
 });
-route ::get('/detail-product', function () {
+route::get('/detail-product', function () {
     return view('client.pages.detail-product');
 });
 
-route ::get('/login', function () {
+route::get('/login', function () {
     return view('client.pages.login');
 });
-route ::get('/register', function () {
+route::get('/register', function () {
     return view('client.pages.register');
 });
-route ::get('/notifications-client', function () {
+route::get('/notifications-client', function () {
     return view('client.pages.notifications-client');
 });
