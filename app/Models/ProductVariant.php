0e016fdf7519b49255b8486a4b5025902fa9a3ed
luongtd15 <luongnek121@gmail.com>
2025-05-28 13:56:56 +0700

    create model ProductVariant

---
+++ app/Models/ProductVariant.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductVariant extends Model
+{
+    //
+    protected $fillable = [
+        'product_id',
+        'color',
+        'size',
+        'status',
+        'price',
+        'stock_quantity',
+        'sku',
+        'image',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
