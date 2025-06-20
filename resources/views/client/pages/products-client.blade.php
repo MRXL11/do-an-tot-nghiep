b786d945b5fb7fa530ea9bfcbca09f37b4dd8c30
Vu Tien Dung <vutiendung8464@gmail.com>
2025-06-20 17:39:25 +0700

    lien ket san pham va sp chi tiet

--- resources/views/client/pages/products-client.blade.php
+++ resources/views/client/pages/products-client.blade.php
@@ -246,7 +246,9 @@
                                         <p class="text-danger fw-bold mb-3">{{ number_format($product->variants->min('price')) }} đ</p>
                                     @endif
                                     <div class="d-flex gap-2 mt-auto justify-content-center">
-                                        <a href="" class="btn btn-outline-primary"><i class="bi bi-eye"></i> Xem</a>
+                                        <a href="{{ route('detail-product', ['id' => $product->id]) }}" class="btn btn-outline-primary">
+                                            <i class="bi bi-eye"></i> Xem
+                                        </a>
                                         <a href="" class="btn btn-outline-danger"><i class="bi bi-cart"></i> Thêm</a>
                                     </div>
                                 </div>
